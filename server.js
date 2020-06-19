d8a4f99e05b1c1bda47aa4079f12eae9b43ff68b
harshakns <harshakns@gmail.com>
2020-06-19 23:57:21 +0530

    fc

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/user", (req, res) => res.send("to be implemented").status(400));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
