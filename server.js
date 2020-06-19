f3f7b1c6bb60d9d4337a60c6b7bc4e2d8f2f8d53
harshakns <harshakns@gmail.com>
2020-06-20 00:42:27 +0530

    fc

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-<<<<<<< HEAD
-app.get("/user", (req, res) => res.send("to be implemented").status(400));
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
-=======
-app.get("/login",(req,res)=>{res.send("to be implemented").status(200)})
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
->>>>>>> 0946490ef3a394ec9d4d805eacf962a53f49572b
