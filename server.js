c85bada6ce6df505b67d15e5ccbdfd617f19b06f
harshakns <harshakns@gmail.com>
2020-06-20 00:53:25 +0530

    local work also added before merge

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const port = 3000;
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/", (req, res) => res.send("secret data received"));
-
+//i also want to create merge conflict
+//local dirty work
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+app.post("/", (req, res) => res.send("secret data received"));
+//local work
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
