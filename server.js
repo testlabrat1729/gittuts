10df360190993a58b98475f10431c1538bd0e4f9
harshakns <harshakns@gmail.com>
2020-06-20 00:49:59 +0530

    new comments

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/", (req, res) => res.send("secret data received"));
 
+//new data to be added
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
