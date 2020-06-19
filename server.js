df2858242ad387ce0d4274a124da035c999709f8
Narasimha Sriharsha KANDURI <18284784+harshakns@users.noreply.github.com>
2020-06-19 23:52:34 +0530

    asfsadf
    asfsadf

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get("/login",(req,res)=>{res.send("to be implemented").status(200)})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
