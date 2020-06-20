69dfc237f99aa7c25fd4240163ba08431e6773bb
harshakns <harshakns@gmail.com>
2020-06-20 10:24:16 +0530

    signup added

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/", (req, res) => res.send("secret data received"));
 
+app.post("/signup", (req, res) => res.send("secret data received"));
+
 //new data to be added
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
