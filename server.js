41552127106aea2b841ea4e962df1a6b3ef95888
harshakns <harshakns@gmail.com>
2020-06-20 18:48:16 +0530

    df:

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 app.get("/", (req, res) => res.send("Hello World!"));
-
 app.post("/", (req, res) => res.send("secret data received"));
-
 app.post("/signup", (req, res) => res.send("secret data received"));
-
 app.post("/signout", (req, res) =>
   res.send("signout not implemented").status(200)
-);
-
-//new data to be added
+); //new data to be added
 //local dirty work
 app.post("/signup", (req, res) => res.send("secret data received"));
 app.post("/home", (req, res) => res.send("secret data received"));
