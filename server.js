41e1a27e902a9ca869310367e72decbddcb33db1
harshakns <harshakns@gmail.com>
2020-06-20 13:37:41 +0530

    fallback

--- server.js
+++ server.js
@@ -12,16 +12,32 @@ app.post("/signout", (req, res) =>
   res.send("signout not implemented").status(200)
 );
 
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-app.post("/", (req, res) => res.send("secret data received"));
-
-//new code to be added
+//new data to be added
+//local dirty work
+app.post("/users", (req, res) => res.send("secret data received"));
+app.post("/signup", (req, res) => res.send("secret data received"));
+app.post("/home", (req, res) => res.send("secret data received"));
+app.post("/aboutus", (req, res) => res.send("secret data received"));
+app.post("/jobs", (req, res) => res.send("secret data received"));
+app.post("/importantInfo", (req, res) => res.send("secret data received"));
+app.post("/404", (req, res) => res.send("secret data received"));
+//very important work
+//need more dirty work
+//asfdsa
+//af
+//asf
+//sadf
+//safd
+//as
+//fas
+//df
+//sdaf
+//sadf
+//sa
+//f
+//asf
+//asf
+//
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
