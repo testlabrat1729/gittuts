41aa1e59ff3d06ee249e296f8d50bc2ae944ad36
harshakns <harshakns@gmail.com>
2020-06-20 19:30:22 +0530

    new changes for new branch

--- models.js
+++ models.js
@@ -2,3 +2,12 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/signout", (req, res) =>
+  res.send("signout not implemented").status(200)
+);
