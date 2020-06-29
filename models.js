68beb161cebcfb215a7ea00c350c02cbe6fd171b
harshakns <harshakns@gmail.com>
2020-06-29 16:20:47 +0530

    user delete route added

--- models.js
+++ models.js
@@ -12,4 +12,7 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/signout", (req, res) =>
   res.send("signout not implemented").status(200)
 );
+app.delete("/user", (req, res) => {
+  res.send("user deleted").status(200);
+});
 //also done by harsha with love
